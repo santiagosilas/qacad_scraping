0366bdd07c53317dcc98f2ace76552c958d006d9
Silas Santiago <santiagosilas@outlook.com>
2019-03-03 22:06:58 +0000

    Example with Headless Chrome

---
+++ qacademico.py
@@ -0,0 +1,46 @@
+
+# Just a module to hold my credetials
+from qsettings import username, password
+import os
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+
+# instantiate a chrome options object so you can set the size and headless preference
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--window-size=1920x1080")
+
+# download the chrome driver from
+# https://sites.google.com/a/chromium.org/chromedriver/downloads
+# and put it in the current directory
+chrome_driver = os.path.join(os.getcwd(), "drivers/chromedriver")
+
+print('Create a Headless Chrome Webdriver..')
+driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver)
+
+
+print('Go to URL...')
+url = 'https://qacademico.ifce.edu.br/qacademico/professores'
+driver.get(url)
+
+print('Filling the login form')
+WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.NAME, 'LOGIN')))
+driver.find_element_by_name('LOGIN').send_keys(username)
+driver.find_element_by_name('SENHA').send_keys(password)
+
+print('Try to access the system..')
+driver.find_element_by_id('btnOk').click()
+
+# capture the start screen
+driver.get_screenshot_as_file("qacademico.png")
+
+# Finally, closes the browser
+driver.close()
+
+print('End!')
